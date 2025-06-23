23f74122660c748ab19b8aad016f2d7c7c77ad9a
Al-Zinan <pappurohan1100@gmail.com>
2025-06-23 06:10:13 +0600

    added withdraw page

---
+++ src/app/admin/withdraw-request/page.js
@@ -0,0 +1,13 @@
+import WithdrawRequest from '@/components/Withdraws/WithdrawRequest'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+        <WithdrawRequest/>
+    </div>
+  )
+}
+
+export default page
